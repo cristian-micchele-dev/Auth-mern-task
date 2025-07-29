512ef961fdbc28a0a202d99b7390cca4016c8d06
cristian <cristian.aiki1@gmail.com>
2025-07-29 11:28:37 -0300

    configurando para produccion

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import app from './app.js';
 import { connectDB } from './db.js';
- 
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
   
-app.listen(4000, () => {
+app.listen(PORT, () => {
   connectDB();
-    console.log('Server running on port 4000');
+  console.log(`Server running on port ${PORT}`);
 });
